09225643a69a7b4d186942a01433433350a5944c
lucian barcan <lucian.barcan98@gmail.com>
2024-06-27 00:22:45 +0300

    Refactorings Part 1

--- client/src/app/components/diagram/diagram.component.ts
+++ client/src/app/components/diagram/diagram.component.ts
@@ -5,6 +5,159 @@ export const ElementType = {
   square: 'square',
 }
 
+export type DiagramElement = {
+  x: number,
+  y: number,
+  width: number,
+  height: number
+}
+
+export enum STATE {
+  IDLE,
+  DRAG
+}
+
+export class Diagram {
+  originalMousePosition = {
+    x: 0,
+    y: 0
+  }
+  private zoomLevel = 4;
+  private elements: DiagramElement[] = [];
+  private topLeftCoordinatesMapping = {
+    x: 0,
+    y: 0
+  }
+  private state = STATE.IDLE;
+
+  private _originalHeight: number | undefined;
+
+  set originalHeight(originalHeight: number | undefined) {
+    if (!originalHeight) throw new Error('Invalid original height');
+    this._originalHeight = originalHeight;
+    this.setCanvasResolution();
+  }
+
+  private _nativeElement: HTMLCanvasElement | undefined;
+
+  get nativeElement() {
+    if (!this._nativeElement) throw new Error('Element not found');
+    return this._nativeElement;
+  }
+
+  set nativeElement(nativeElement: HTMLCanvasElement) {
+    this._nativeElement = nativeElement
+    if (this.elements.length) this.redraw()
+  }
+
+  get context() {
+    const context = this.nativeElement.getContext('2d');
+    if (!context) throw new Error('Context not found');
+    return context
+  }
+
+  drawElement(element: DiagramElement) {
+    const context = this.nativeElement.getContext('2d');
+    const x = element.x + this.topLeftCoordinatesMapping.x;
+    const y = element.y + this.topLeftCoordinatesMapping.y;
+    if (context) {
+      context.fillStyle = 'blue';
+      context.fillRect(x, y, element.width, element.height);
+    }
+  }
+
+  loadElements(elements: DiagramElement[]) {
+    this.elements = elements;
+    if (!this._nativeElement) return;
+    this.redraw();
+  }
+
+  drawGrid() {
+    const gridOffsetX = this.topLeftCoordinatesMapping.x % 100;
+    const gridOffsetY = this.topLeftCoordinatesMapping.y % 100;
+
+    const pixelsX = Math.floor(Math.abs(this.topLeftCoordinatesMapping.x) / 100);
+    const pixelsY = Math.floor(Math.abs(this.topLeftCoordinatesMapping.y) / 100);
+
+    if (this.zoomLevel < 5) {
+      for (let i = gridOffsetX, indexX = 0; i < this.nativeElement.width; i += 100, indexX++) {
+        for (let j = gridOffsetY, indexY = 0; j < this.nativeElement.height; j += 100, indexY++) {
+          this.context.beginPath();
+          const radius = ((indexX - pixelsX) + (indexY - pixelsY)) % 2 === 0 ? 8 : 6;
+          this.context.arc(i, j, radius, 0, 2 * Math.PI);
+          this.context.fillStyle = ((indexX - pixelsX) + (indexY - pixelsY)) % 2 === 0 ? 'darkgray' : 'darkgray';
+          this.context.fill();
+        }
+      }
+    }
+  }
+
+  redraw(attributes?: { setResolution?: boolean, drawGrid?: boolean }) {
+    const setResolution = attributes?.setResolution ?? false;
+    const drawGrid = attributes?.drawGrid ?? true;
+
+    if (setResolution) this.setCanvasResolution();
+    this.clear();
+    if (drawGrid) this.drawGrid();
+    this.drawElements();
+  }
+
+  onDragEnter(event: PointerEvent) {
+    if (this.state === STATE.DRAG) return;
+    this.state = STATE.DRAG;
+    this.nativeElement.style.cursor = 'grabbing';
+    this.originalMousePosition = {
+      x: event.clientX,
+      y: event.clientY
+    }
+  }
+
+  onEnterIdleState() {
+    if (this.state === STATE.IDLE) return;
+    this.nativeElement.style.cursor = 'default';
+    this.state = STATE.IDLE;
+  }
+
+  onMouseMove(event: MouseEvent) {
+    switch (this.state) {
+      case STATE.IDLE:
+        return;
+      case STATE.DRAG:
+        const dx = event.clientX - (this.originalMousePosition.x || 0);
+        const dy = event.clientY - (this.originalMousePosition.y || 0);
+        this.originalMousePosition = {
+          x: event.clientX,
+          y: event.clientY
+        }
+        this.topLeftCoordinatesMapping = {
+          x: this.topLeftCoordinatesMapping.x + dx * this.zoomLevel,
+          y: this.topLeftCoordinatesMapping.y + dy * this.zoomLevel
+        }
+        this.redraw();
+        break;
+    }
+  }
+
+  private clear() {
+    this.context.clearRect(0, 0, this.nativeElement.width, this.nativeElement.height);
+  }
+
+  private drawElements() {
+    this.elements.forEach(element => this.drawElement(element));
+  }
+
+  private setCanvasResolution() {
+    const parent = this.nativeElement.parentElement;
+
+    if (parent) {
+      this.nativeElement.width = parent.clientWidth * this.zoomLevel;
+      this.nativeElement.height = (this._originalHeight || 1) * this.zoomLevel;
+    }
+
+    this.redraw();
+  }
+}
+
 @Component({
   selector: 'app-diagram',
   standalone: true,
@@ -15,32 +168,25 @@ export const ElementType = {
   styleUrl: './diagram.component.scss'
 })
 export class DiagramComponent implements AfterViewInit, OnInit {
-  @ViewChild('diagramCanvas') diagramCanvas: ElementRef<HTMLCanvasElement> | undefined;
   currentlySelectedNewElement: string | undefined;
   elementType = ElementType;
-  originalMousePosition: { x: number | undefined, y: number | undefined } = {
-    x: undefined,
-    y: undefined
-  }
-  private elements: { x: number, y: number, width: number, height: number }[] = [];
-  private topLeftCoordinatesMapping = {
-    x: 0,
-    y: 0
-  }
-  private drag: boolean = false;
-  private zoomFactor = 4;
-  private originalHeight: number | undefined;
-  private mouseDown = false;
 
-  get nativeElement(): HTMLCanvasElement {
-    const nativeElement = this.diagramCanvas?.nativeElement;
-    if (!nativeElement) throw new Error('Native element not found');
-    return nativeElement;
+  private _diagram = new Diagram();
+
+  get diagram(): Diagram {
+    if (!this._diagram) throw new Error('Diagram not found');
+    return this._diagram;
+  }
+
+  @ViewChild('diagramCanvas') set diagram(diagramCanvas: ElementRef<HTMLCanvasElement>) {
+    if (diagramCanvas) {
+      this._diagram.nativeElement = diagramCanvas.nativeElement;
+    }
   }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
-    this.setCanvasResolution();
+    this.diagram.redraw({setResolution: true});
   }
 
   @HostListener('contextmenu', ['$event'])
@@ -50,7 +196,7 @@ export class DiagramComponent implements AfterViewInit, OnInit {
 
   @HostListener('body:wheel', ['$event'])
   onWheel(event: WheelEvent) {
-    if (event.target !== this.nativeElement) return;
+    if (event.target !== this._diagram?.nativeElement) return;
     if (event.deltaY > 0) {
       this.onZoomOut();
     } else {
@@ -61,24 +207,13 @@ export class DiagramComponent implements AfterViewInit, OnInit {
   ngOnInit() {
     const persistedElements = localStorage.getItem('elements');
     if (persistedElements) {
-      this.elements = JSON.parse(persistedElements);
+      const elements = JSON.parse(persistedElements);
+      this.diagram.loadElements(elements);
     }
   }
 
   ngAfterViewInit(): void {
-    this.originalHeight = this.nativeElement.parentElement?.clientHeight;
-    this.setCanvasResolution();
-  }
-
-  setCanvasResolution() {
-    const parent = this.nativeElement.parentElement;
-
-    if (parent) {
-      this.nativeElement.width = parent.clientWidth * this.zoomFactor;
-      this.nativeElement.height = (this.originalHeight || 1) * this.zoomFactor;
-    }
-
-    this.redrawCanvas();
+    this.diagram.originalHeight = this.diagram.nativeElement.parentElement?.clientHeight;
   }
 
   onSelectItem(text: string) {
@@ -89,120 +224,73 @@ export class DiagramComponent implements AfterViewInit, OnInit {
     this.currentlySelectedNewElement = undefined;
   }
 
-  redrawCanvas() {
-    const context = this.nativeElement.getContext('2d');
-    if (!context) return;
-    context.clearRect(0, 0, this.nativeElement.width, this.nativeElement.height);
-
-    const gridOffsetX = this.topLeftCoordinatesMapping.x % 100;
-    const gridOffsetY = this.topLeftCoordinatesMapping.y % 100;
-
-    const pixelsX = Math.floor(Math.abs(this.topLeftCoordinatesMapping.x) / 100);
-    const pixelsY = Math.floor(Math.abs(this.topLeftCoordinatesMapping.y) / 100);
-
-    if (this.zoomFactor < 5) {
-      for (let i = gridOffsetX, indexX = 0; i < this.nativeElement.width; i += 100, indexX++) {
-        for (let j = gridOffsetY, indexY = 0; j < this.nativeElement.height; j += 100, indexY++) {
-          context.fillStyle = ((indexX - pixelsX) + (indexY - pixelsY)) % 2 === 0 ? 'darkgray' : 'dimgray';
-          context.fillRect(i, j, 12, 12);
-        }
-      }
-    }
-
-    this.elements.forEach(element => {
-      context.fillStyle = 'blue';
-      const x = element.x + this.topLeftCoordinatesMapping.x;
-      const y = element.y + this.topLeftCoordinatesMapping.y;
-      context?.fillRect(x, y, element.width, element.height);
-    })
-  }
-
-  drawSquare(x: number, y: number) {
-    const rectSize = 200; // Square size
-
-    const context = this.nativeElement.getContext('2d');
-    if (context) {
-      context.fillStyle = 'blue';
-      context.fillRect(x - this.nativeElement.offsetLeft - rectSize / 2, y - this.nativeElement.offsetTop - rectSize / 2, rectSize, rectSize);
-      this.elements.push({
-        x: x - this.nativeElement.offsetLeft - rectSize / 2 - this.topLeftCoordinatesMapping.x,
-        y: y - this.nativeElement.offsetTop - rectSize / 2 - this.topLeftCoordinatesMapping.y,
-        width: rectSize,
-        height: rectSize
-      });
-    }
-  }
+  // drawSquare(x: number, y: number) {
+  //   const rectSize = 200; // Square size
+  //
+  //   const context = this.nativeElement.getContext('2d');
+  //   if (context) {
+  //     context.fillStyle = 'blue';
+  //     context.fillRect(x - this.nativeElement.offsetLeft - rectSize / 2, y - this.nativeElement.offsetTop - rectSize / 2, rectSize, rectSize);
+  //     this.elements.push({
+  //       x: x - this.nativeElement.offsetLeft - rectSize / 2 - this.topLeftCoordinatesMapping.x,
+  //       y: y - this.nativeElement.offsetTop - rectSize / 2 - this.topLeftCoordinatesMapping.y,
+  //       width: rectSize,
+  //       height: rectSize
+  //     });
+  //   }
+  // }
 
   onCanvasClick(event: MouseEvent) {
-    if (this.currentlySelectedNewElement) {
-      const rect = this.nativeElement.getBoundingClientRect();
-      const x = event.clientX - rect.left;
-      const y = event.clientY - rect.top;
-      switch (this.currentlySelectedNewElement) {
-        case ElementType.square:
-          this.drawSquare(x * this.zoomFactor, y * this.zoomFactor);
-          break;
-      }
-    }
-
-    this.onSave();
+    //   if (this.currentlySelectedNewElement) {
+    //     const rect = this.nativeElement.getBoundingClientRect();
+    //     const x = event.clientX - rect.left;
+    //     const y = event.clientY - rect.top;
+    //     switch (this.currentlySelectedNewElement) {
+    //       case ElementType.square:
+    //         this.drawSquare(x * this.zoomFactor, y * this.zoomFactor);
+    //         break;
+    //     }
+    //   }
+    //
+    //   this.onSave();
   }
 
   onCanvasMouseDown(event: MouseEvent) {
-    this.mouseDown = true;
-    this.originalMousePosition = {
-      x: event.clientX,
-      y: event.clientY
-    }
+    const pointerEvent = event as PointerEvent;
+    if (event.ctrlKey) this.diagram.onDragEnter(pointerEvent);
   }
 
   onCanvasMouseMove(event: MouseEvent) {
-    if (this.mouseDown) {
-      this.nativeElement.style.cursor = 'grabbing';
-      this.drag = true;
-      const dx = event.clientX - (this.originalMousePosition.x || 0);
-      const dy = event.clientY - (this.originalMousePosition.y || 0);
-      this.originalMousePosition = {
-        x: event.clientX,
-        y: event.clientY
-      }
-      this.topLeftCoordinatesMapping = {
-        x: this.topLeftCoordinatesMapping.x + dx * this.zoomFactor,
-        y: this.topLeftCoordinatesMapping.y + dy * this.zoomFactor
-      }
-      this.redrawCanvas();
-    }
+    this.diagram.onMouseMove(event);
   }
 
-  onCanvasMouseUp(event: MouseEvent) {
-    this.mouseDown = false;
-    if (!this.drag) {
-      this.onCanvasClick(event);
-      return;
-    }
-    this.drag = false;
-    this.nativeElement.style.cursor = 'default';
+  onCanvasMouseUp() {
+    this.diagram.onEnterIdleState();
   }
 
   onMouseLeave() {
-    this.drag = false;
-    this.mouseDown = false;
-    this.nativeElement.style.cursor = 'default';
+    this.diagram.onEnterIdleState();
   }
 
   onZoomIn() {
-    if (this.zoomFactor < 0.7) return;
-    this.zoomFactor -= 0.05;
-    this.setCanvasResolution();
+    // if (this.zoomFactor < 0.7) return;
+    // this.zoomFactor -= 0.05;
+    // this.setCanvasResolution();
   }
 
   onZoomOut() {
-    if (this.zoomFactor > 6) return;
-    this.zoomFactor = this.zoomFactor + 0.05;
-    this.setCanvasResolution();
+    // if (this.zoomFactor > 6) return;
+    // this.zoomFactor = this.zoomFactor + 0.05;
+    // this.setCanvasResolution();
+  }
+
+  onReset() {
+    // localStorage.removeItem('elements');
+    // this.elements = [];
+    // this.redrawCanvas();
   }
 
   private onSave() {
-    localStorage.setItem('elements', JSON.stringify(this.elements));
+    // localStorage.setItem('elements', JSON.stringify(this.elements));
   }
 }
